455f680b8dab68fc3868353cb78204ac19b1475d
VertoDebru <t.vervoot@gmail.com>
2022-09-13 21:06:08 +0200

    Add Loader component.

---
+++ front/src/components/Loader/Loader.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+// Import style.
+import './Loader.css';
+
+export default class Loader extends React.Component {
+
+    render() {
+        return (
+            <div id="Wrap-Loader">
+                <div className='App-logo'>
+                    <i className="bx bxs-star"></i>
+                </div>
+            </div>
+        );
+    }
+}
